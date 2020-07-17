1609a7ec03bfdbaa1d1a4dd643479992a9c5f35f
ryokosuge <ryo.kosuge@gmail.com>
2020-07-17 13:57:17 +0900

    feat:	storeを繋ぎこむ

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
-import Counter from './pages/counter';
+import { CounterPage as Counter } from './pages/counter';
 
 import logo from './logo.svg';
 import './App.css';
@@ -13,7 +13,7 @@ const App: React.FunctionComponent = () => {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Counter count={0} />
+          <Counter />
         </header>
       </div>
     </Provider>
